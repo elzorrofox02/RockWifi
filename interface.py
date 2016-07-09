dcd8fb417b68c221e7cf40d13b5ed81f7720041e
Miguelangel <elzorrofox02@gmail.com>
2016-07-09 04:41:42 -0430

    interfaces2

--- interface.py
+++ interface.py
@@ -1,20 +1,31 @@
+from subprocess import Popen,PIPE
+import os
+
 class Interfaces():
-	def iwconfig():
+	def iwconfig(self):
 		monitors = []
 		interfaces = {}
-		proc = Popen(['iwconfig'], stdout=PIPE, stderr=DN)
+		num = 0
+		proc = Popen(['iwconfig'], stdout=PIPE, stderr=PIPE)
 		for line in proc.communicate()[0].split('\n'):
 			if len(line) == 0: continue # Isn't an empty string
 			if line[0] != ' ': # Doesn't start with space
-				#ignore_iface = re.search('eth[0-9]|em[0-9]|p[1-9]p[1-9]|at[0-9]', line)
-				#if not ignore_iface: # Isn't wired or at0 tunnel
+				
 				iface = line[:line.find(' ')] # is the interface name
 				if 'Mode:Monitor' in line:
 					monitors.append(iface)
-				elif 'IEEE 802.11' in line:
-					if "ESSID:\"" in line:
-						interfaces[iface] = 1
-					else:
-						interfaces[iface] = 0
+				elif 'IEEE 802.11' in line:										
+					numer = num+1
+					interfaces[numer] = iface
+						
+		
+		for clave,valor in interfaces.items():
+			print "%s) %s"%(clave,valor)			
+		elige1 = raw_input(" Elige Interface > ")
+		seleccion = interfaces.get(int(elige1))
+
+		print "Haz Selecionado %s"%seleccion
+
+
 		return monitors, interfaces
-interfaces().iwconfig()
+Interfaces().iwconfig()
