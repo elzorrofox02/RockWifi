ca4665830d2a35014c03d4bb0efcb9a34e4b2fd4
Miguelangel <elzorrofox02@gmail.com>
2016-07-15 15:31:31 -0430

    Server4

--- server.py
+++ server.py
@@ -1,49 +1,75 @@
-from http.server import HTTPServer,SimpleHTTPRequestHandler,BaseHTTPRequestHandler
+try:
+	from http.server import HTTPServer,SimpleHTTPRequestHandler,BaseHTTPRequestHandler
+except ImportError:
+	from BaseHTTPServer import HTTPServer
+	from SimpleHTTPServer import SimpleHTTPRequestHandler
+	from BaseHTTPServer import BaseHTTPRequestHandler
 import threading
 import ssl
-
-class HandlerSecure(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/plain")
-		self.end_headers()
-		message = "Hello world!"
-		self.wfile.write(bytes(message, "utf8"))	
-
-
-	def log_message(self, format, *args):
-		return
 
 class ServerSecure(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 		HandlerClass = HandlerSecure
 		ServerClass = HTTPServer
-		self.httpd = ServerClass(('127.0.0.1', 443), HandlerClass)
-		self.httpd.socket = ssl.wrap_socket (self.httpd.socket,keyfile="server.pem",certfile='server.pem', server_side=True)
-
-		#Forma 1
-		#self.socket = ssl.wrap_socket (self.socket, certfile='server.pem', server_side=True)
-		#ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-		#ctx.load_cert_chain(certfile="server.pem", keyfile="server.pem")
-		#self.socket = ctx.wrap_socket(socket.socket(self.address_family, self.socket_type), server_side=True)
-		#self.server_bind()
-		#self.server_activate()
+		self.httpd = ServerClass(('0.0.0.0', 443), HandlerClass)
+		self.httpd.socket = ssl.wrap_socket (self.httpd.socket,keyfile="othes/server.pem",certfile='othes/server.pem', server_side=True)
 
 	def run(self):
+		print ("Corriendo 443")
 		self.httpd.serve_forever()
 
 	def shutdown(self):
 		self.httpd.shutdown()
 		self.httpd.socket.close()
 
+class HandlerSecure(BaseHTTPRequestHandler):
+	def do_GET(self):		
+		self.send_response(301)
+		#self.send_header('Location', 'http://' + NETWORK_GW_IP + ':' + str(PORT))
+		self.send_header('Location', 'http://127.0.0.1')
+		self.end_headers()	
+
+	def log_message(self, format, *args):
+		return
+
+class ServerHttpd(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		HandlerClass = HandlerHttpd
+		ServerClass = HTTPServer
+		self.httpd = ServerClass(('0.0.0.0', 80), HandlerClass)
+
+	def run(self):
+		print ("Corriendo 80")
+		self.httpd.serve_forever()
+
+	def shutdown(self):
+		self.httpd.shutdown()
+		self.httpd.socket.close()
+
+class HandlerHttpd(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/plain")
+		self.end_headers()
+		message = "Hello world 80!"
+		self.wfile.write(bytes(message, "utf8"))
+
+	def log_message(self, format, *args):
+		return
 
 hola = ServerSecure()
 hola.start()
 
+hola2 = ServerHttpd()
+hola2.start()
+
 xd = input("ingrea :: ")
 if xd == "1":
 	hola.shutdown()
+	hola2.shutdown()
+
 
 
 
